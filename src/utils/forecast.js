67082779914416343170a68bd532f1c50d25feb4
Adam Paul <adumb42@gmail.com>
2021-02-24 19:56:28 +0000

    Change what weather API pulls

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,15 @@
 const request = require('request');
 
+// 
+// Goal: Add new data to forecast
+// 
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to Github and deploy to Heroku
+// 4. Test you work in the live application
+
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=1e1d27d27169f10b63898e523bc10989&query=' + latitude + ',' + longitude + 'units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=1e1d27d27169f10b63898e523bc10989&query=' + latitude + ',' + longitude + 'units=f'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +17,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + ' % chance of rain.')
+            callback(undefined, 'There is currently ' + body.current.weather_descriptions[0].toLowerCase() + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + '. The humidity is at ' + body.current.humidity + '.')
         }
     })
 }
